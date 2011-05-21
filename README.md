b2fabaadab57ed80a7965002c847385075517ec1
Kevin Sylvestre <kevin@ksylvest.com>
2011-05-21 12:54:06 -0400

    initial import

---
+++ README.md
@@ -0,0 +1,20 @@
+# jQuery Growler
+
+Growler is a jQuery plugin designed to provide messages.
+
+## Installation
+
+To install copy the *images*, *javascripts*, and *stylesheets* directories into your project and add the following snippet to the header:
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js" type="text/javascript"></script>
+    <script src="javascripts/jquery.growler.js" type="text/javascript"></script>
+    <link href="stylesheets/style.css" rel="stylesheet" type="text/css" />
+  
+## Examples
+
+    $.growler.growl({ title: "Growl", message: "The kitten is awake!" });
+
+# Copyright
+
+Copyright (c) 2011 Kevin Sylvestre. See LICENSE for details.
---
+++ javascripts/jquery.growler.js
@@ -0,0 +1,88 @@
+/*!
+ * jQuery Growler
+ *
+ * Copyright 2010 - 2011 Kevin Sylvestre
+ */
+
+(function ($) {
+	
+	$.growler = { options: { style: 'growls br' } };
+	
+	$.growler.growl = function(options) {
+		
+		var settings = {
+      speed: 400,
+			duration: 3200,
+			opacity: 0.8,
+			close: '&times;',
+			style: 'default',
+			size: 'medium',
+			incoming: { opacity: 0.80 },
+			outgoing: { opacity: 0.00 },
+    };
+
+	  var options = $.extend(settings, options);  
+		
+		var html = '<div id="growler" />';
+    $('body:not(:has(#growler))').append(html);
+
+		var $growler = $('#growler');
+		$growler.attr('class', $.growler['options']['style']);
+
+		var $growl = $('<div class="growl" />');
+		
+		$growl.attr('class', settings['style'] + ' growl ' + settings['size']);
+		
+		var $close   = $('<div class="close" />').html(options['close']);
+		var $title   = $('<div class="title" />').html(options['title']);
+		var $message = $('<div class="message" />').html(options['message']);
+		
+		$growl.append($close);
+		$growl.append($title);
+		$growl.append($message);
+		
+		$growl.css({ opacity: 0.0, position: "relative" });
+		
+		$growler.append($growl);
+		
+		$growl
+			.animate(options.incoming, options.speed)
+			.delay(options.duration)
+			.animate(options.outgoing, options.speed)
+		;
+	};
+	
+	$.growler.error = function(options) {
+		
+		var settings = {
+			title: 'Error!',
+			style: 'error',
+		}
+		
+		$.growler.growl($.extend(settings, options));
+		
+	};
+	
+	$.growler.notice = function(options) {
+		
+		var settings = {
+			title: 'Notice!',
+			style: 'notice',
+		}
+		
+		$.growler.growl($.extend(settings, options));
+		
+	};
+	
+	$.growler.warning = function(options) {
+		
+		var settings = {
+			title: 'Warning!',
+			style: 'warning',
+		}
+		
+		$.growler.growl($.extend(settings, options));
+		
+	};
+    
+}) (jQuery);
