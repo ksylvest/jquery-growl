089ff1f4f974819c77080b1881b093bd4c2943ec
Kevin Sylvestre <kevin@ksylvest.com>
2013-08-25 12:05:03 -0700

    added a fix for IE

--- javascripts/jquery.growl.coffee
+++ javascripts/jquery.growl.coffee
@@ -1,7 +1,7 @@
 ###
 jQuery Growl
 Copyright 2013 Kevin Sylvestre
-1.1.1
+1.1.2
 ###
 
 "use strict"
@@ -12,7 +12,6 @@ class Animation
   @transitions:
     "webkitTransition": "webkitTransitionEnd"
     "mozTransition": "mozTransitionEnd"
-    "msTransition": "msTransitionEnd"
     "oTransition": "oTransitionEnd"
     "transition": "transitionend"
 
--- javascripts/jquery.growl.js
+++ javascripts/jquery.growl.js
@@ -2,7 +2,7 @@
 /*
 jQuery Growl
 Copyright 2013 Kevin Sylvestre
-1.1.1
+1.1.2
 */
 
 
@@ -19,7 +19,6 @@ Copyright 2013 Kevin Sylvestre
     Animation.transitions = {
       "webkitTransition": "webkitTransitionEnd",
       "mozTransition": "mozTransitionEnd",
-      "msTransition": "msTransitionEnd",
       "oTransition": "oTransitionEnd",
       "transition": "transitionend"
     };
